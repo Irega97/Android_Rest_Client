df893c0edcfb66f794063f04ccc77cfa7f6cf551
irega97 <ivanreq97@gmail.com>
2020-04-11 20:14:55 +0200

    Get, put y delete implementados. Falta revisar POST (edit).

--- app/src/main/java/edu/upc/dsa/recyclerviewexample/EditTrack.java
+++ app/src/main/java/edu/upc/dsa/recyclerviewexample/EditTrack.java
@@ -2,13 +2,76 @@ package edu.upc.dsa.recyclerviewexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class EditTrack extends AppCompatActivity {
+    private API api;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_track);
+
+        //Crear retrofit
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://147.83.7.203:8080/dsaApp/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        //Llamamos a servicios que hemos definido en la API
+        api = retrofit.create(API.class);
+
+        //Declaramos intent para recuperar parametros y volver al Main
+        final Intent i = getIntent();
+        final Intent main_intent = new Intent(EditTrack.this, MainActivity.class);
+
+        //Definimos botones y editores de texto
+        Button update = (Button) findViewById(R.id.update_btn);
+        final EditText id_text = findViewById(R.id.track_id);
+        final EditText title_text = findViewById(R.id.track_title);
+        final EditText singer_text = findViewById(R.id.track_singer);
+
+        id_text.setText(i.getStringExtra("id"));
+        title_text.setText(i.getStringExtra("title"));
+        singer_text.setText(i.getStringExtra("singer"));
+
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newID = id_text.getText().toString();
+                String newTitle = title_text.getText().toString();
+                String newSinger = singer_text.getText().toString();
+
+                Track updatedTrack = new Track (newID,newTitle,newSinger);
+                updateTrack(updatedTrack);
+            }
+        });
+
+    }
+
+    public void updateTrack(Track t){
+        Call<Void> call = api.updateTrack(t);
+
+        call.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+
+            }
+        });
     }
 }
--- app/src/main/java/edu/upc/dsa/recyclerviewexample/TrackActivity.java
+++ app/src/main/java/edu/upc/dsa/recyclerviewexample/TrackActivity.java
@@ -11,19 +11,38 @@ import android.widget.Toast;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class TrackActivity extends AppCompatActivity {
+private API api;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_track);
 
+        //Crear retrofit
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://147.83.7.203:8080/dsaApp/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        //Llamamos a servicios que hemos definido en la API
+        api = retrofit.create(API.class);
+
         //Recuperamos datos que nos pasa el intent al pulsar el track (en MyAdapter)
         //para que nos muestre los detalles del track
-        Intent i = getIntent();
-        TextView idTV = findViewById(R.id.track_id);
-        TextView titleTV = findViewById(R.id.track_title);
-        TextView singerTV = findViewById(R.id.track_singer);
+        final Intent i = getIntent();
+        final Intent edit_intent = new Intent(TrackActivity.this, EditTrack.class);
+
+        //Inicializamos TextViews
+        final TextView idTV = findViewById(R.id.track_id);
+        final TextView titleTV = findViewById(R.id.track_title);
+        final TextView singerTV = findViewById(R.id.track_singer);
 
         //Añadimos info que nos ha pasado a los textviews correspondientes
         idTV.setText(i.getStringExtra("id"));
@@ -38,8 +57,11 @@ public class TrackActivity extends AppCompatActivity {
         edit_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(TrackActivity.this, EditTrack.class);
-                startActivity(i);
+                //Pasamos info del track para poder editarla
+                edit_intent.putExtra("id", idTV.getText().toString());
+                edit_intent.putExtra("title", titleTV.getText().toString());
+                edit_intent.putExtra("singer", singerTV.getText().toString());
+                startActivity(edit_intent);
             }
         });
 
@@ -48,12 +70,15 @@ public class TrackActivity extends AppCompatActivity {
         del_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //Pop-up de confirmación
                 AlertDialog myQuittingDialogBox = new AlertDialog.Builder(TrackActivity.this)
+                        //Titulo, mensaje y botones del pop-up
                         .setTitle("Borrar")
                         .setMessage("Seguro que quieres borrar el track?")
                         .setPositiveButton("Delete", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialogInterface, int i) {
+                                deleteTrack(idTV.getText().toString());
                                 Intent intent_main = new Intent (TrackActivity.this, MainActivity.class);
                                 startActivity(intent_main);
                             }
@@ -69,6 +94,26 @@ public class TrackActivity extends AppCompatActivity {
                         .create();
                 myQuittingDialogBox.create();
                 myQuittingDialogBox.show();
+            }
+        });
+    }
+
+    public void deleteTrack(String id){
+        Call<Void> call = api.deleteTrack(id);
+
+        call.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                if(response.isSuccessful())
+                    Toast.makeText(getApplicationContext(), "Track borrado", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                Toast toast = Toast.makeText(getApplicationContext(), "Error al acceder a la API", Toast.LENGTH_SHORT);
+                toast.show();
             }
         });
     }
