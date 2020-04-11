44acd77c06880a21912a94bf0ef5a4b00f6ed838
irega97 <ivanreq97@gmail.com>
2020-04-11 06:15:42 +0200

    Clase API completa

--- app/src/main/java/edu/upc/dsa/recyclerviewexample/API.java
+++ app/src/main/java/edu/upc/dsa/recyclerviewexample/API.java
@@ -1,4 +1,34 @@
 package edu.upc.dsa.recyclerviewexample;
 
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+
 public interface API {
+
+    public interface Tracks_API {
+
+        @GET("tracks")
+        Call<List<Track>> getTracks();
+
+        @POST("tracks")
+        Call<Track> saveTrack (@Body Track track);
+
+        @PUT("tracks")
+        Call<Void> updateTrack (@Body Track track);
+
+        @DELETE("tracks/{id}")
+        Call<Void> deleteTrack (@Path("id") String id);
+
+    }
 }
